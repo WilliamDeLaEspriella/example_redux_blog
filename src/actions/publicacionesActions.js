f3576014918956140200e03b9f88a32efd3e0607
William De la Espriella <wdelaespriella@hotmail.com>
2019-08-20 09:26:26 -0500

    add params to actions

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 import { TRAER_TODOS, CARGANDO, ERROR } from "../types/publicacionesType";
 export const traerPublicaciones = () => async dispatch => {
-    dispatch({ type: CARGANDO});
+  dispatch({ type: CARGANDO });
   try {
     const result = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
+    );
+    dispatch({ type: TRAER_TODOS, payload: result.data });
+  } catch (error) {
+    dispatch({ type: ERROR, error: error.message });
+  }
+};
+
+export const traerPorUsuario = key => async (dispatch, getState) => {
+  const { usuarios } = getState().usuarioReducer;
+  const usuarios_id = usuarios[key].id;
+  dispatch({ type: CARGANDO });
+  try {
+    const result = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${usuarios_id}`
     );
     dispatch({ type: TRAER_TODOS, payload: result.data });
   } catch (error) {
--- src/components/publicaciones/index.js
+++ src/components/publicaciones/index.js
@@ -1,43 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
 import * as publicacionesActions from "../../actions/publicacionesActions";
-import Spinner from '../general/Spinner';
-import Fatal from '../general/Fatal';
+import Spinner from "../general/Spinner";
+import Fatal from "../general/Fatal";
 class Publicaciones extends Component {
-    componentDidMount(){
-        if(!this.props.usuarioReducer.usuarios.length){
-            this.props.traerTodos()
-            this.props.traerPublicaciones()
-        }
+  async componentDidMount() {
+    if (!this.props.usuarioReducer.usuarios.length) {
+      await this.props.traerTodos();
     }
-    render() {
-        console.log(this.props)
-        if (this.props.cargando) {
-            return (
-              <center>
-                <Spinner />
-              </center>
-            );
-          }
-          if (this.props.error) {
-            return <Fatal error={this.props.error} />;
-          }
-        return (
-            <div>
-                <h1>Pulicaciones de</h1>
-                {this.props.match.params.key}
-            </div>
-        );
+    this.props.traerPorUsuario(this.props.match.params.key);
+  }
+  render() {
+    console.log(this.props);
+    if (this.props.cargando) {
+      return (
+        <center>
+          <Spinner />
+        </center>
+      );
     }
+    if (this.props.error) {
+      return <Fatal error={this.props.error} />;
+    }
+    return (
+      <div>
+        <h1>Pulicaciones de</h1>
+        {this.props.match.params.key}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = ({usuarioReducer,publicaionesReducer}) => {
-    return{usuarioReducer,publicaionesReducer}};
-const mapDispacherToPorps={
-    ...usuariosActions,
-    ...publicacionesActions
-}
+const mapStateToProps = ({ usuarioReducer, publicaionesReducer }) => {
+  return { usuarioReducer, publicaionesReducer };
+};
+const mapDispacherToPorps = {
+  ...usuariosActions,
+  ...publicacionesActions
+};
 export default connect(
   mapStateToProps,
   mapDispacherToPorps
